56b992232fd8f33f7454943688647d95d8f8ccd6
Mohammed Imran <108051979+mechmdim4an@users.noreply.github.com>
2022-07-20 10:58:50 +0530

    Details of codes / comments added to calculator.js file

--- calculator.js
+++ calculator.js
@@ -1,24 +1,30 @@
 //jshint esversion:6
+// express module and body-parser
 
-const express = require("express");
-const bodyParser = require("body-parser")
+const express = require("express");             //Express module
+const bodyParser = require("body-parser")       //npm body-parser module
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 
+//home page callback function for simple addition  
 app.get("/", function (req, res) {
     res.sendFile(__dirname + "/index.html");
 });
+
+//BMI Calculator page callback function 
 app.get("/bmiCalculator", function (req, res) {
     res.sendFile(__dirname + "/bmiCalculator.html");
 })
+
+// Simple Addition 
 app.post("/", function (req, res) {
-    var num1 = Number(req.body.num1);
+    var num1 = Number(req.body.num1); 
     var num2 = Number(req.body.num2);
     var addResult = num1 + num2;
     res.send("Your calculated value is:" + addResult);
 });
 
-
+// BMI calculation BMI = weight / height^2 (kg/cm^2 or kg/m^2)
 app.post("/bmiCalculator", function (req, res) {
     var weight = parseFloat(req.body.weight);
     var height = parseFloat(req.body.height);
@@ -26,6 +32,7 @@ app.post("/bmiCalculator", function (req, res) {
     res.send("Your BMI value is: " + bmi);
 });
 
+// creating server port 3000
 app.listen(3000, function () {
     console.log("Server Start on port 3000")
 });
